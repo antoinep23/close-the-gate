80fd2a5a2983bb21a45c308f6aa39abf2364af21
Antoine Papyn <antoine.papyn@devoteam.com>
2026-08-04 10:57:23 +0200

    implement other cli functions

--- src/cli.ts
+++ src/cli.ts
@@ -55,5 +55,72 @@ program
   .action((options) => {
     const file = new File();
     const key = new Key();
-    // to implement
+    
+    try {
+      const retrieved = key.retrieve(options.key);
+      if (retrieved instanceof Error) {
+        throw retrieved;
+      }
+
+      const uploaded = file.upload(options.file, key);
+      if (uploaded instanceof Error) {
+        throw uploaded;
+      }
+
+      console.log("File uploaded successfully");
+    } catch(e) {
+      console.error("Error uploading file: ", e);
+    }
   })
+
+  program
+  .command("delete-file")
+  .description("Delete an encrypted file from your AWS S3")
+  .requiredOption("-f, --file <fileName>", "Name of the file to delete (located inside the /files folder)")
+  .requiredOption("-k, --key <keyName>", "Name of the key file to use for deletion, located inside the /keys folder")
+  .action((options) => {
+    const file = new File();
+    const key = new Key();
+    
+    try {
+      const retrieved = key.retrieve(options.key);
+      if (retrieved instanceof Error) {
+        throw retrieved;
+      }
+
+      const deleted = file.delete(options.file, key);
+      if (deleted instanceof Error) {
+        throw deleted;
+      }
+
+      console.log("File deleted successfully");
+    } catch(e) {
+      console.error("Error deleting file: ", e);
+    }
+  });
+
+program
+  .command("download-file")
+  .description("Download an encrypted file from your AWS S3")
+  .requiredOption("-f, --file <fileName>", "Name of the file to download (located inside the /files folder)")
+  .requiredOption("-k, --key <keyName>", "Name of the key file to use for download, located inside the /keys folder")
+  .action((options) => {
+    const file = new File();
+    const key = new Key();
+    
+    try {
+      const retrieved = key.retrieve(options.key);
+      if (retrieved instanceof Error) {
+        throw retrieved;
+      }
+
+      const downloaded = file.download(options.file, key);
+      if (downloaded instanceof Error) {
+        throw downloaded;
+      }
+
+      console.log("File downloaded successfully");
+    } catch(e) {
+      console.error("Error downloading file: ", e);
+    }
+  });
--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,6 @@
 /// <reference types="node" />
 ///
 import "dotenv/config";
-// import Key from "./keys"
-// import File from "./files"
-
-// const key = new Key;
-// // const bytes = 32; // must be between 16 and 64 bytes
-// // key.generate(bytes);
-// key.retrieve("010c0295-4c46-4b70-8768-b1c4c461f72f.pem");
-
-// const file = new File();
-// // file.upload("100Mb.dat", key);
-// // file.download("100Mb.dat", key);
-// // file.delete("100Mb.dat", key);
-
 import { program } from "./cli";
 
 program.parse(process.argv);
